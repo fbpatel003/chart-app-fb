b37cc273f0d515eeb2ead654ae492293d48ea54f
Fenil Patel <102594553+fbpatel003@users.noreply.github.com>
2023-01-03 11:23:51 +0530

    11230301CPC

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,7 +17,7 @@ import Select from "@mui/material/Select";
 import { SvgIcon } from "@mui/material";
 
 function Home(props) {
-  const [chartTF, setChartTF] = React.useState("");
+  const [chartTF, setChartTF] = React.useState(props.chartData.TimeFrame);
 
   const handleChangeTF = (event) => {
     setChartTF(event.target.value);
@@ -251,6 +251,23 @@ function Home(props) {
     </SvgIcon>
   );
 
+  const usdIcon = (
+    <SvgIcon>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path fill="#f0f0f0" d="M0 85.331h512v341.337H0z" />
+        <path
+          fill="#d80027"
+          d="M0 127.994h512v42.663H0zM0 213.331h512v42.663H0zM0 298.657h512v42.663H0zM0 383.994h512v42.663H0z"
+        />
+        <path fill="#2e52b2" d="M0 85.331h256v183.797H0z" />
+        <path
+          fill="#f0f0f0"
+          d="m99.822 160.624-4.123 12.684H82.363l10.791 7.835-4.123 12.683 10.791-7.835 10.784 7.835-4.122-12.683 10.791-7.835h-13.337zM103.938 219.08l-4.116-12.683-4.123 12.683H82.363l10.791 7.836-4.123 12.683 10.791-7.836 10.784 7.836-4.122-12.683 10.791-7.836zM47.577 219.08l-4.117-12.683-4.123 12.683H26.001l10.791 7.836-4.123 12.683 10.791-7.836 10.785 7.836-4.122-12.683 10.789-7.836zM43.46 160.624l-4.123 12.684H26.001l10.791 7.835-4.123 12.683 10.791-7.835 10.785 7.835-4.122-12.683 10.789-7.835H47.577zM99.822 114.85l-4.123 12.685H82.363l10.791 7.836-4.123 12.683 10.791-7.836 10.784 7.836-4.122-12.683 10.791-7.836h-13.337zM43.46 114.85l-4.123 12.685H26.001l10.791 7.836-4.123 12.683 10.791-7.836 10.785 7.836-4.122-12.683 10.789-7.836H47.577zM156.183 160.624l-4.122 12.684h-13.336l10.79 7.835-4.121 12.683 10.789-7.835 10.786 7.835-4.123-12.683 10.791-7.835h-13.336zM160.301 219.08l-4.118-12.683-4.122 12.683h-13.336l10.79 7.836-4.121 12.683 10.789-7.836 10.786 7.836-4.123-12.683 10.791-7.836zM216.663 219.08l-4.117-12.683-4.123 12.683h-13.335l10.789 7.836-4.122 12.683 10.791-7.836 10.785 7.836-4.123-12.683 10.791-7.836zM212.546 160.624l-4.123 12.684h-13.335l10.789 7.835-4.122 12.683 10.791-7.835 10.785 7.835-4.123-12.683 10.791-7.835h-13.336zM156.183 114.85l-4.122 12.685h-13.336l10.79 7.836-4.121 12.683 10.789-7.836 10.786 7.836-4.123-12.683 10.791-7.836h-13.336zM212.546 114.85l-4.123 12.685h-13.335l10.789 7.836-4.122 12.683 10.791-7.836 10.785 7.836-4.123-12.683 10.791-7.836h-13.336z"
+        />
+      </svg>
+    </SvgIcon>
+  );
+
   const list = (anchor) => (
     <>
       <Box
@@ -288,6 +305,7 @@ function Home(props) {
             { pair: "GBP", icon: gbpIcon },
             { pair: "NZD", icon: nzdIcon },
             { pair: "JPY", icon: jpyIcon },
+            { pair: "USD", icon: usdIcon },
           ].map((text, index) => (
             <Link
               style={{ color: "grey", textDecoration: "none" }}
@@ -303,6 +321,7 @@ function Home(props) {
             </Link>
           ))}
         </List>
+        <Divider />
       </Box>
       <div style={{ width: "80%", margin: "auto", marginTop: "25px" }}>
         <div>
@@ -380,7 +399,6 @@ function Home(props) {
               <em>None</em>
             </MenuItem>
             <MenuItem value={5}>5M</MenuItem>
-            <MenuItem value={10}>10M</MenuItem>
             <MenuItem value={15}>15M</MenuItem>
             <MenuItem value={30}>30M</MenuItem>
             <MenuItem value={60}>1H</MenuItem>
